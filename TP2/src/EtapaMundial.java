31bbebd4f7dff0b8de32a78c8b59c52cd6a915c1
tatiAndonegui <115169557+tatiAndonegui@users.noreply.github.com>
2022-11-18 18:20:46 -0300

    empezanding con etapa mundial

---
+++ TP2/src/EtapaMundial.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class EtapaMundial {
+    private String descripcionEtapa;
+    private ArrayList <Partido> partidos;
+
+    public String getDescripcionEtapa() {
+        return descripcionEtapa;
+    }
+}
